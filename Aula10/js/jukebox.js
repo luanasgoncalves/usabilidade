fee2e7d4a25de588b9b2567e50f13e3b873d7d30
Luana Gonçalves <144810083+luanasgoncalves@users.noreply.github.com>
2025-05-23 20:25:15 -0300

    jukebox

---
+++ Aula10/js/jukebox.js
@@ -0,0 +1,97 @@
+const form = document.getElementById('formMusicas');
+const inputMusica = document.getElementById('musica');
+const listaMusicas = document.getElementById('listaMusicas');
+const btnTocar = document.getElementById('tocarMusica');
+const tocandoDiv = document.getElementById('tocando');
+const inputCodigo = document.getElementById('codigoMusica');
+const btnBuscar = document.getElementById('btnPesquisar');
+
+const musicas = [];
+const MAX_MUSICAS = 8;
+
+//pesquisa pelo código da música
+btnBuscar.addEventListener('click', function() {
+  const codigo = inputCodigo.value.trim();
+  if (!/^\d{4}$/.test(codigo)) {
+    alert('Por favor, digite um código válido de 4 dígitos.');
+    inputCodigo.value = '';
+    return;
+  }
+
+  //if (bancoMusicas.hasOwnPropety(codigo)) {
+   // inputMusica.value = bancoMusicas[codigo];
+   // inputMusica.focus();
+//  } else {
+//  alert('Código não encontrado no banco de músicas.'); 
+
+//  }
+
+if (bancoMusicas.hasOwnProperty(codigo)) {
+  inputMusica.value = bancoMusicas[codigo];
+  inputMusica.focus();
+}else{
+  alert('Código não encontrado no banco de músicas.');
+  inputMusica.value = '';
+  inputCodigo.value ='';
+
+}
+});
+
+//adiciona a musica ao input
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    if (musicas.length >= MAX_MUSICAS) {
+      alert('Maximo de musicas');
+      inputCodigo.value = '';
+      inputMusica.value = '';
+      return;
+    }
+
+    const musicaDigitada = inputMusica.value.trim();
+
+    if (musicaDigitada === '') {
+        alert('Por favor, digite o nome da música.');
+        return;
+      }
+
+    if(musicas.includes(musicaDigitada)) {
+      const posicao = musicas.indexOf(musicaDigitada) + 1;
+      alert(`A música "${musicaDigitada}" já está na fila na posição ${posicao}.`);
+      inputCodigo.value = '';
+      inputMusica.value = '';
+      inputCodigo.focus();
+      return;
+    }
+
+    musicas.push(musicaDigitada);
+    atualizarLista();
+    inputCodigo.value ='';
+    inputMusica.value = '';
+    inputMusica.focus();
+});
+
+function atualizarLista() {
+    listaMusicas.innerHTML = '';
+    musicas.forEach(function(musica) {
+      const li = document.createElement('li');
+      li.textContent = musica;
+      listaMusicas.appendChild(li);
+    });
+  }
+
+  btnTocar.addEventListener('click', function() {
+    if (musicas.length === 0) {
+      tocandoDiv.innerHTML = `PLAYLIST VAZIO!`;
+      alert('A lista está vazia, não há músicas para tocar.');
+      return;
+    }
+
+    const musicaTocando = musicas.shift(); // Remove a primeira música
+    atualizarLista();
+    tocandoDiv.innerHTML = `Tocando a música:<br><marquee>${musicaTocando}</marquee>`;
+    tocandoDiv.style.display = 'block';
+
+  });
+
+  //LIMITA A PLAYLIST EM 8 MÚSICAS
